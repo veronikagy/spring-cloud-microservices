355b45088668fee05118567c9d837a6b1e94db74
Поляков Дмитрий Игоревич <dpolyakov@mmtr.ru>
2024-05-08 15:11:22 +0300

    add application

---
+++ gateway/src/main/java/com/javastart/GatewayApplication.java
@@ -0,0 +1,8 @@
+package com.javastart;
+
+public class GatewayApplication {
+
+  public static void main(String[] args) {
+
+  }
+}
---
+++ notification-service/src/main/java/com/javastart/notificaton/NotificatonApplication.java
@@ -0,0 +1,7 @@
+package com.javastart.notificaton;
+
+public class NotificatonApplication {
+  public static void main(String[] args) {
+
+  }
+}
